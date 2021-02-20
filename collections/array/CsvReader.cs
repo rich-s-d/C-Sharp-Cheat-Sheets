681d075877da86d6edc86d5c6233669d3e96b5d6
GIS-enthusiast <srichnz@gmail.com>
2021-02-20 16:37:44 +0100

    first push

---
+++ collections/array/CsvReader.cs
@@ -0,0 +1,40 @@
+
+namespace collections
+{
+    public class CsvReader
+    {
+    private string _csvFilePath;
+    
+        public CsvReader(string csvFilePath)
+        {
+            _csvFilePath = csvFilePath;
+        }
+
+        public Country[] ReadFirstNCountries(int nCountries)
+        {
+            Country[] countries = new Country[nCountries];
+            // using (StreamReader reader = new StreamReader(_csvFilePath))
+            // {
+            //     reader.Readline();
+            //     for (int i = 0; i < nCountries; i++)
+            //     {
+            //         string csvLine = reader.Readline();
+            //         countries[i] = ReadCountryFromCsvLine(csvLine);
+            //     }
+            // }
+            return countries;
+        }
+
+        public Country ReadCountryFromCsvLine(string csvLine)
+        {
+            string[] parts = csvLine.Split(',');
+
+            string name = parts[0];
+            string code = parts[1];
+            string region = parts[2];
+            int population = int.Parse(parts[3]);
+
+            return new Country(name, code, region, population);
+        }
+    }
+}
---
+++ collections/array/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // string filePath = "";
+            // CsvReader reader = new CsvReader(filePath);
+            // Country[] countries = reader.ReadFirstNCountries(10);
+
+            // foreach(Country country in countries)
+            // {
+            //     System.Console.WriteLine($"{country.Population}: {country.Name}");
+            // }
+            int[] arrayOfValueTypes = new int[4];
+            Country[] arrayOfRefTypes = new Country[1];
+
+            foreach (var item in arrayOfValueTypes)
+            {
+                System.Console.WriteLine(item);
+            }
+            foreach (Country country in arrayOfRefTypes)
+            {
+                System.Console.WriteLine(country.Name);
+            }
+        }
+    }
+}
